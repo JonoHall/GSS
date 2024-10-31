0382226ca3068fb0278fceb883cdffa5bdac7f57
Jono Hall <jonohall@gmail.com>
2024-11-01 09:23:47 +1300

    Update tamper.js

--- tamper.js
+++ tamper.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         GoSweetSpot AutoFill
 // @namespace    http://tampermonkey.net/
-// @version      1.1
+// @version      1.2
 // @description  try to take over the world!
 // @author       JH
 // @match        https://*/EcommOrderImport/View?ecommerceOrderImportPK=*
